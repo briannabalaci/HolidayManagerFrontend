ef37b6192958b290deda67a6053eb11b3c4f54db
Adrian Buciuman <adrian.buciuman@mhp.com>
2021-08-04 14:20:42 +0300

    removed additional folder app

--- EventManager-frontend/src/app/app.module.ts
+++ EventManager-frontend/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './app/components/main/main.component';
-import { MainModule } from './app/components/main/main.module';
-import { DashboardComponent } from './app/components/dashboard/dashboard.component';
+import { MainComponent } from './components/main/main.component';
+import { MainModule } from './components/main/main.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EventCardComponent } from './app/components/dashboard/event-card/event-card.component';
+import { EventCardComponent } from './components/dashboard/event-card/event-card.component';
 
 @NgModule({
   declarations: [
