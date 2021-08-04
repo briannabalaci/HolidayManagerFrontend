865cf847aafde713c6d1694e361df3229299710a
Mihai Andrei <Mihai.Andrei2@mhp.com>
2021-08-04 15:54:53 +0300

    resolved one more conflict

--- EventManager-frontend/src/app/app.module.ts
+++ EventManager-frontend/src/app/app.module.ts
@@ -3,11 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainModule } from './components/main/main.module';
-<<<<<<< HEAD
-import {​​​​​​​​ BrowserAnimationsModule }​​​​​​​​ from '@angular/platform-browser/animations';
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';;
->>>>>>> user-admin-page
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
