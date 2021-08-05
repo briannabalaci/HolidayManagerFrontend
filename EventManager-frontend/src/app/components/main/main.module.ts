32dd831d51087afca20c403648479eaf779b8f26
zjakab <zsanett.jakab@mhp.com>
2021-08-05 12:14:26 +0300

    login style

--- EventManager-frontend/src/app/components/main/main.module.ts
+++ EventManager-frontend/src/app/components/main/main.module.ts
@@ -33,8 +33,7 @@ const routes: Routes = [
     DashboardModule,
     UserAdminModule,
     MatTableModule,
-    MatFormFieldModule,
-    MatButtonModule
+    MatFormFieldModule
   ],
   exports: [
     MainComponent
