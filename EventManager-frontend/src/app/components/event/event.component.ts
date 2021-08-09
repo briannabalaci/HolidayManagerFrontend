9a5e09748951d5a3bc423435ce8c3491b97b3886
DESKTOP-CJTKSNG\TestIntern02 <andra.suciu@mhp.com>
2021-08-09 15:09:35 +0300

    header + invitees

--- EventManager-frontend/src/app/components/event/event.component.ts
+++ EventManager-frontend/src/app/components/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,7 +9,16 @@ import { Router } from '@angular/router';
 })
 export class EventComponent implements OnInit {
 
-  constructor() { }
+  eventFormGroup = this.formBuilder.group({
+    title: ['', Validators.required],
+    event_date: ['', Validators.required],
+    event_time: ['', Validators.required],
+    location: ['', Validators.required],
+    dress_code: ['', Validators.required],
+    cover_image: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
--- EventManager-frontend/src/app/components/event/event.module.ts
+++ EventManager-frontend/src/app/components/event/event.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventComponent } from './event.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxMatDatetimePickerModule } from '@angular-material-components/datetime-picker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFileUploadModule } from 'angular-material-fileupload';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
 
 const routes: Routes = [
   {
@@ -16,7 +26,20 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    NgxMatDatetimePickerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFileUploadModule,
+    NgxMatFileInputModule
+  ],
+  providers: [  
+    NgxMatDatetimePickerModule,  
   ],
   exports: [EventComponent]
 })
