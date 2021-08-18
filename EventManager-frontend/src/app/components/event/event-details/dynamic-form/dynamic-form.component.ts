f44b57c674930a023ff1a7a0b7e63aa0f90e39c1
Mihai Andrei <Mihai.Andrei2@mhp.com>
2021-08-18 15:54:14 +0300

    event attendee buttons working

--- EventManager-frontend/src/app/components/event/event-details/dynamic-form/dynamic-form.component.ts
+++ EventManager-frontend/src/app/components/event/event-details/dynamic-form/dynamic-form.component.ts
@@ -7,6 +7,9 @@ import { InviteQuestionResponse } from 'src/app/shared/data-types/invite-questio
 import { Question } from 'src/app/shared/data-types/question';
 import { InviteService } from 'src/app/shared/services/invite.service';
 import { QuestionControlService } from 'src/app/shared/services/question-control.service';
+import {Location} from '@angular/common';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -17,18 +20,26 @@ export class DynamicFormComponent implements OnInit {
 
   form!: FormGroup;
   payLoad = '';
+  verif: any;
+  role: string = '';
+  token: string = '';
 
   constructor(private questionControlService: QuestionControlService,
-              private inviteService: InviteService) { }
+              private inviteService: InviteService,
+              private route: Router) { }
 
   @Input() questions: Question[] = [];
   @Input() invite?: Invite ;
+  @Input() response?: InviteQuestionResponse;
 
   status: string = 'Not answered';
 
   ngOnInit() {
     this.form = this.questionControlService.toFormGroup(this.questions);
     this.form.reset();
+    this.token = sessionStorage.getItem('token')!;
+    this.role = jwt_decode<any>(this.token).roles[1];
+    console.log(this.invite?.status);
   }
 
   onSubmit() {
@@ -66,10 +77,54 @@ export class DynamicFormComponent implements OnInit {
 
   onAccept() {
     this.status = "accepted";
+    //this.route.navigate(['dashboard']);
   }
 
   onDecline() {
     this.status = "declined";
+    //this.route.navigate(['dashboard']);
+    // this.inviteService.deleteResponse(this.invite?.id?).subscribe(
+    //   data => {
+
+    //   }
+    // )
+    
   }
+
+
+  onAcceptStatus(): boolean {
+    if(this.invite?.status === "accepted") {
+      this.verif = true;
+    } else {
+      this.verif = false;
+    }
+    return this.verif;
+  }
+
+  onDeclineStatus(): boolean {
+    if(this.invite?.status === "declined") {
+      this.verif = true;
+    } else {
+      this.verif = false;
+    }
+    return this.verif;
+  }
+
+  onNotAccepted(): boolean {
+    if(this.invite?.status === "Not answered") {
+      this.verif = true;
+    } else {
+      this.verif = false;
+    }
+    return this.verif;
+  }
+
+  onCancelAttendee() {
+    this.route.navigate(['dashboard']);
+  }
+
+  // onUpdate() {
+  //   this.inviteService.updateResponses(this.invite?.inviteQuestionResponses?);
+  // }
 
 }
--- EventManager-frontend/src/app/shared/services/invite.service.ts
+++ EventManager-frontend/src/app/shared/services/invite.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Invite } from '../data-types/invite';
+import { InviteQuestionResponse } from '../data-types/invite-question-response';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,20 @@ export class InviteService {
     const path = `${this.ENVIRONMENT}/update`;
     return this.httpClient.put<Invite>(path, invite);
   }
+
+  getEventStatus(): Observable<Invite[]> {
+    const path = `${this.ENVIRONMENT}/getByStatus/${status}`;
+    return this.httpClient.get<Invite[]>(path);
+  }
+
+  updateResponses(response: InviteQuestionResponse): Observable<any> {
+    const path = `${this.ENVIRONMENT}/inviteQuestionResponse/update`
+    return this.httpClient.put<InviteQuestionResponse>(path, response);
+  }
+
+  deleteResponse(id: number): Observable<any> {
+    const path = `${this.ENVIRONMENT}/inviteQuestionResponse/delete/${id}`;
+    return this.httpClient.delete<InviteQuestionResponse>(path);
+  }
+
 }
