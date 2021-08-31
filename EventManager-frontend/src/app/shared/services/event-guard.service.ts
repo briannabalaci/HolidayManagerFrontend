e4f9fa1df6545fc7fe3c27824f30bfa46d4ea652
Adrian Buciuman <adrian.buciuman@mhp.com>
2021-08-31 11:38:29 +0300

    Event guard working properly

--- EventManager-frontend/src/app/shared/services/event-guard.service.ts
+++ EventManager-frontend/src/app/shared/services/event-guard.service.ts
@@ -5,7 +5,7 @@ import { EventService } from './event.service';
 import jwt_decode from 'jwt-decode';
 import { Observable } from 'rxjs';
 import { EventEntity } from '../data-types/event';
-import { map } from 'rxjs/operators';
+import { map, takeWhile } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,12 +17,15 @@ export class EventGuardService implements CanActivate {
   canActivate(router: ActivatedRouteSnapshot): Observable<boolean> {
     const eventId = router.params.eventId;
     const token: any = jwt_decode(sessionStorage.getItem('token') || '');
+    console.log(eventId);
     
     return this.eventService.getEventsByUserIdAndFilter(token.email, "All Events").pipe(
         map(
             (data: EventEntity[]) => {
+            
             for(const event of data) {
-                if(event.id === eventId) {
+                console.log(event.id + ' ' + eventId);
+                if(event.id == eventId || event.organizer === token.email) {
                     return true;
                 }
             }
