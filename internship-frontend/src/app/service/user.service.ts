ab91657c519765685f0248885ef9e6bda41fe31c
DESKTOP-CJTKSNG\TestIntern02 <tara-adela.hudrea@mhp.com>
2022-08-03 11:21:58 +0300

    no message

--- internship-frontend/src/app/service/user.service.ts
+++ internship-frontend/src/app/service/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const LOGIN = "http://localhost:8090/user/login-user"
+const LOGIN = "http://localhost:8090/user/login"
 
 export class UserLoginData{
   email?:string;
@@ -17,7 +17,7 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   public login(loginData: UserLoginData): Observable<String>{
-    console.log("Aici in service")
+    console.log("Ajung in service")
     return this.httpClient.post<String>(LOGIN,loginData);
   }
 }
